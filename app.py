1d875af300058b0b3339dcaec4c53355cffbfd1b
goswamiayush <43089408+goswamiayush@users.noreply.github.com>
2025-12-20 10:48:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def get_gemini_response(user_query):
         )
 
         response = client.models.generate_content(
-            model='gemini-2.0-flash', 
+            model='gemini-2.5-flash', 
             contents=f"User Query: {user_query}. Provide specific fees in INR/USD and deadlines.",
             config=generate_config
         )
