552a159e28f6eb0aa5e72895104c0d8a92e47b6f
goswamiayush <43089408+goswamiayush@users.noreply.github.com>
2025-12-20 14:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def get_gemini_response(query, history):
         """
 
         response = client.models.generate_content(
-            model='gemini-3-flash',
+            model='gemini-2.5-flash',
             contents=query,
             config=types.GenerateContentConfig(
                 temperature=0.3,
